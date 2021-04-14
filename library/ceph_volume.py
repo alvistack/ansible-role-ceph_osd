8396bc4bb355c697c7e7bc36ba59670ead63d203
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2021-04-14 15:58:12 +0800

    https://github.com/ceph/ceph/pull/40845

--- library/ceph_volume.py
+++ library/ceph_volume.py
@@ -183,32 +183,115 @@ EXAMPLES = '''
 '''
 
 
+def generate_ceph_cmd(sub_cmd, args, user_key=None, cluster='ceph', user='client.admin', container_image=None, interactive=False):  # noqa: E501
+    '''
+    Generate 'ceph' command line to execute
+    '''
+
+    if not user_key:
+        user_key = '/etc/ceph/{}.{}.keyring'.format(cluster, user)
+
+    cmd = pre_generate_ceph_cmd(container_image=container_image, interactive=interactive)  # noqa: E501
+
+    base_cmd = [
+        '-n',
+        user,
+        '-k',
+        user_key,
+        '--cluster',
+        cluster
+    ]
+    base_cmd.extend(sub_cmd)
+    cmd.extend(base_cmd + args)
+
+    return cmd
+
+
+def container_exec(binary, container_image, interactive=False):
+    '''
+    Build the docker CLI to run a command inside a container
+    '''
+
+    container_binary = os.getenv('CEPH_CONTAINER_BINARY')
+    command_exec = [container_binary, 'run']
+    if interactive:
+        command_exec.extend(['--interactive'])
+
+        command_exec.extend([
+                '--rm',
+                '--net=host',
+                '-v', '/etc/ceph:/etc/ceph:z',
+                '-v', '/var/lib/ceph/:/var/lib/ceph/:z',
+                '-v', '/var/log/ceph/:/var/log/ceph/:z',
+                '--entrypoint=' + binary, container_image
+        ])
+    return command_exec
+
+
+def is_containerized():
+    '''
+    Check if we are running on a containerized cluster
+    '''
+
+    if 'CEPH_CONTAINER_IMAGE' in os.environ:
+        container_image = os.getenv('CEPH_CONTAINER_IMAGE')
+    else:
+        container_image = None
+
+    return container_image
+
+
+def pre_generate_ceph_cmd(container_image=None, interactive=False):
+    '''
+    Generate ceph prefix comaand
+    '''
+    if container_image:
+        cmd = container_exec('ceph', container_image, interactive=interactive)
+    else:
+        cmd = ['ceph']
+
+    return cmd
+
+
+def exec_command(module, cmd, stdin=None):
+    '''
+    Execute command(s)
+    '''
+
+    binary_data = False
+    if stdin:
+        binary_data = True
+    rc, out, err = module.run_command(cmd, data=stdin, binary_data=binary_data)
+
+    return rc, cmd, out, err
+
+
+def exit_module(module, out, rc, cmd, err, startd, changed=False):
+    endd = datetime.datetime.now()
+    delta = endd - startd
+
+    result = dict(
+        cmd=cmd,
+        start=str(startd),
+        end=str(endd),
+        delta=str(delta),
+        rc=rc,
+        stdout=out.rstrip("\r\n"),
+        stderr=err.rstrip("\r\n"),
+        changed=changed,
+    )
+    module.exit_json(**result)
+
+
 def fatal(message, module):
     '''
     Report a fatal error and exit
     '''
 
     if module:
-        module.fail_json(msg=message, changed=False, rc=1)
+        module.fail_json(msg=message, rc=1)
     else:
         raise(Exception(message))
-
-
-def container_exec(binary, container_image):
-    '''
-    Build the docker CLI to run a command inside a container
-    '''
-    container_binary = os.getenv('CEPH_CONTAINER_BINARY')
-    command_exec = [container_binary, 'run',
-                    '--rm', '--privileged', '--net=host', '--ipc=host',
-                    '-v', '/run/lock/lvm:/run/lock/lvm:z',
-                    '-v', '/var/run/udev/:/var/run/udev/:z',
-                    '-v', '/dev:/dev', '-v', '/etc/ceph:/etc/ceph:z',
-                    '-v', '/run/lvm/:/run/lvm/',
-                    '-v', '/var/lib/ceph/:/var/lib/ceph/:z',
-                    '-v', '/var/log/ceph/:/var/log/ceph/:z',
-                    '--entrypoint=' + binary, container_image]
-    return command_exec
 
 
 def build_cmd(action, container_image, cluster='ceph', binary='ceph-volume'):
@@ -231,28 +314,6 @@ def build_cmd(action, container_image, cluster='ceph', binary='ceph-volume'):
     cmd.extend(action)
 
     return cmd
-
-
-def exec_command(module, cmd):
-    '''
-    Execute command
-    '''
-
-    rc, out, err = module.run_command(cmd)
-    return rc, cmd, out, err
-
-
-def is_containerized():
-    '''
-    Check if we are running on a containerized cluster
-    '''
-
-    if 'CEPH_CONTAINER_IMAGE' in os.environ:
-        container_image = os.getenv('CEPH_CONTAINER_IMAGE')
-    else:
-        container_image = None
-
-    return container_image
 
 
 def get_data(data, data_vg):
@@ -279,7 +340,7 @@ def get_wal(wal, wal_vg):
     return wal
 
 
-def batch(module, container_image):
+def batch(module, container_image, report=None):
     '''
     Batch prepare OSD devices
     '''
@@ -310,7 +371,8 @@ def batch(module, container_image):
     action = ['lvm', 'batch']
     cmd = build_cmd(action, container_image, cluster)
     cmd.extend(['--%s' % objectstore])
-    cmd.append('--yes')
+    if not report:
+        cmd.append('--yes')
 
     if container_image:
         cmd.append('--prepare')
@@ -475,10 +537,11 @@ def is_lv(module, vg, lv, container_image):
 
     rc, cmd, out, err = exec_command(module, cmd)
 
-    result = json.loads(out)['report'][0]['lv']
-    if rc == 0 and len(result) > 0:
+    if rc == 0:
+        result = json.loads(out)['report'][0]['lv']
+    if len(result) > 0:
         return True
-    else:
+
         return False
 
 
@@ -676,7 +739,7 @@ def run_module():
             '--format=json',
         ]
 
-        cmd = batch(module, container_image)
+        cmd = batch(module, container_image, report=True)
         batch_report_cmd = copy.copy(cmd)
         batch_report_cmd.extend(report_flags)
 
